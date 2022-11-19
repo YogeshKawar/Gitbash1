baf2fb0e2f2949196ff1c68d3d0acd5d522e680c
YogeshKawar <ykawar00@gmail.com>
2022-11-19 18:59:25 +0530

    fifth commit

--- src/main/java/Demo/A.java
+++ src/main/java/Demo/A.java
@@ -1,5 +1,25 @@
 package Demo;
 
 public class A {
-
+public static void main(String[] args) {
+	int sumOfCube= 0;
+	int a,temp;
+	int number = 153;
+	temp=number;
+	
+	while(number>0) 
+	{
+		a=number%10;//Getting last digit
+		number=number/10;//removing last digit from number
+		sumOfCube = sumOfCube+(a*a*a);//Calculating sum of digits
+	}
+	if (temp==sumOfCube) 
+	{
+		System.out.println(temp+" Is armstrong number");
+	}
+	else
+	{
+		System.out.println(temp+" Is not armstrong number");
+	}
+}
 }
