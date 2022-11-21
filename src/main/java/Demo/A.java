594e307ea03b4f25c7ff79bcdac74e6f8d0caa2c
DeepakCigniti <deepakkhetred03@gmail.com>
2022-11-21 15:50:50 +0530

    Changes made in A by Deepak

--- src/main/java/Demo/A.java
+++ src/main/java/Demo/A.java
@@ -2,24 +2,15 @@ package Demo;
 
 public class A {
 public static void main(String[] args) {
-	int sumOfCube= 0;
-	int a,temp;
-	int number = 153;
-	temp=number;
-	
-	while(number>0) 
-	{
-		a=number%10;//Getting last digit
-		number=number/10;//removing last digit from number
-		sumOfCube = sumOfCube+(a*a*a);//Calculating sum of digits
-	}
-	if (temp==sumOfCube) 
-	{
-		System.out.println(temp+" Is armstrong number");
-	}
-	else
-	{
-		System.out.println(temp+" Is not armstrong number");
+	int n1=0;
+	int n2=1;
+	int sum=0;
+	System.out.print(n1+" "+n2+" ");
+	for(int i=1;i<=10;i++) {
+		sum=n1+n2;
+		System.out.print(sum+" ");
+		n1=n2;
+		n2=sum;
 	}
 }
 }
