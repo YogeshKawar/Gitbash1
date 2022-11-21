f3b817521b64136c8d42b580cb8d63758f618327
YogeshKawar <ykawar00@gmail.com>
2022-11-21 16:03:02 +0530

    changes after merge

--- src/main/java/Demo/A.java
+++ src/main/java/Demo/A.java
@@ -2,6 +2,7 @@ package Demo;
 
 public class A {
 public static void main(String[] args) {
+<<<<<<< HEAD
 	int n1 =0, n2=1, n3,i,count=10;
 	System.out.print(n1+" "+n2);
 
@@ -10,6 +11,17 @@ public static void main(String[] args) {
 		System.out.print(" "+n3);
 		n1=n2;
 		n2=n3;
+=======
+	int n1=0;
+	int n2=1;
+	int sum=0;
+	System.out.print(n1+" "+n2+" ");
+	for(int i=1;i<=10;i++) {
+		sum=n1+n2;
+		System.out.print(sum+" ");
+		n1=n2;
+		n2=sum;
+>>>>>>> 594e307ea03b4f25c7ff79bcdac74e6f8d0caa2c
 	}
 }
 }
